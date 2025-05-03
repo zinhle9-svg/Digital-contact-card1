e06d2bd50e76087bd813a7236525a5f03d2f709c
zinhle <ngwanejeanette3@gmail.com>
2025-05-03 04:28:11 +0200

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Digital contact card
+        </p>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ContactCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContactCard
+
